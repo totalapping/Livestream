498a38964a6675bf7e250b4a10e9b635942befa8
totalapping <hemantyadav9484@gmail.com>
2025-04-01 15:41:48 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -30,9 +30,9 @@ while true; do
   
   ffmpeg -re -stream_loop -1 -i "/app/video.mp4" \
     -c:v libx264 -preset veryfast -b:v 2500k -maxrate 2500k -bufsize 5000k \
-    -vf "scale=1280:720,fps=30" -g 60 -keyint_min 30 \
+    -vf "scale=720:1280,fps=30" -g 60 -keyint_min 30 \
     -c:a aac -b:a 128k -ar 44100 \
-    -f flv "rtmp://a.rtmp.youtube.com/live2/2ytm-kw83-gwbh-ch8p-fxhd" 2>> /app/ffmpeg.log
+    -f flv "rtmp://a.rtmp.youtube.com/live2/y0f9-1dxx-ppjq-pfh3-40u6" 2>> /app/ffmpeg.log
   
   # If stream crashes
   echo "[$(date)] Stream crashed! Restarting in 5 seconds..." >> /app/stream.log
